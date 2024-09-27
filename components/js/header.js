1f44849e519a73e1476fb7f76e91b8465b16a2a2
BrunoCTmb <zazaduza10@gmail.com>
2024-09-27 01:05:32 -0300

    html and css for header

---
+++ components/js/header.js
@@ -0,0 +1,26 @@
+const NAV_BAR_CONTENT = `
+    <header>
+        <div class="logo">
+            <h1>Logo</h1>
+        </div>
+
+        <ul class="project-header-links">
+            <a href="#"><li>Início</li></a>
+            <a href="#"><li>Projeto</li></a>
+            <a href="#"><li>Sobre</li></a>
+        </ul>
+        
+        <ul class="app-header-links">
+            <a href="#"><li>Campeonatos</li></a>
+            <a href="#"><li>Times</li></a>
+            <a href="#"><li>Ranking</li></a>
+        </ul>
+        
+        <ul class="auth-header">
+            <a href="#"><li>Login</li></a>
+            <a href="#"><li>Cadastro</li></a>
+        </ul>
+    </header>
+` 
+
+document.getElementById('header').innerHTML = NAV_BAR_CONTENT;
--- components/js/navbar.js
+++
@@ -1,7 +0,0 @@
-const NAV_BAR_CONTENT = `
-    <nav>
-        nav test
-    </nav>
-` 
-
-document.getElementById('navbar').innerHTML = NAV_BAR_CONTENT;
