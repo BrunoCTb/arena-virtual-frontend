23501720e35b66889e177b34343163c7bf5d04c7
BrunoCTmb <zazaduza10@gmail.com>
2024-10-12 01:07:51 -0300

    'balls' follow showing content

--- static/js/api/create-tournament.js
+++ static/js/api/create-tournament.js
@@ -5,28 +5,35 @@
  * 
 */
 function changeLevel(event) {
-    let arr = [];
-
     // os 3 itens que serao navegados
-    arr = document.getElementsByClassName("create-process");
+    let arr = document.getElementsByClassName("create-process");
+    let balls = document.getElementsByClassName("process-visualize");
+
 
     for (let i = 0; i < arr.length; i++) {
         if (event.target.id == "advance-level") {
             console.log("advance");
             next = arr[i + 1] || arr[0];
+            nextBall = balls[i + 1] || balls[0];
+            
         } else if (event.target.id == "return-level") {
             console.log("return");
             next = arr[i - 1] || arr[arr.length - 1];
+            nextBall = balls[i - 1] || balls[balls.length - 1];
         }
 
         if (arr[i].style.display != "none") {
             arr[i].style.display = "none";
             next.style.display = "flex";
+
+            nextBall.style.backgroundColor = "#fff";
+            balls[i].style.backgroundColor = "#ffffff94";
+
             break
         }
-
     }
 }
+
 returnBtn = document.getElementById("return-level");
 advanceBtn = document.getElementById("advance-level");
 
