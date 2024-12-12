f64235d6b8a7f8e6e64d679a24e32db8b74c671b
BrunoCTmb <zazaduza10@gmail.com>
2024-12-11 22:53:27 -0300

    add level 2 layout and add/remove for formats form

--- static/js/api/create-tournament.js
+++ static/js/api/create-tournament.js
@@ -1,13 +1,9 @@
 // SETAR OS NÍVEIS DE CRIAÇÃO DE CAMPEONATO
-
-/**
- * @param {number} level // ex: 1, 2 ou 3
- * 
-*/
 function changeLevel(event) {
     // os 3 itens que serao navegados
     let arr = document.getElementsByClassName("create-process");
     let balls = document.getElementsByClassName("process-visualize");
+    const showDisplay = "flex";
 
 
     for (let i = 0; i < arr.length; i++) {
@@ -24,7 +20,7 @@ function changeLevel(event) {
 
         if (arr[i].style.display != "none") {
             arr[i].style.display = "none";
-            next.style.display = "flex";
+            next.style.display = showDisplay;
 
             nextBall.style.backgroundColor = "#fff";
             balls[i].style.backgroundColor = "#ffffff94";
@@ -39,3 +35,37 @@ advanceBtn = document.getElementById("advance-level");
 
 advanceBtn.addEventListener("click", changeLevel);
 returnBtn.addEventListener("click", changeLevel);
+
+
+// mudar quantidade de form dependendo de quantos formatos forem selecionados
+function selectFormat() {
+    // criar um novo form
+    
+
+    
+}
+
+// adicionar ou remover formatos - de 1 (pelo menos) até 3
+
+const FORMAT_QUANTITY = 3
+const removeFormatBtn = document.getElementById("remove-format");
+const addFormatBtn = document.getElementById("add-format");
+const formats = document.getElementsByClassName("format");
+
+let currentFormat = 0;
+
+removeFormatBtn.addEventListener("click", formatsConfig)
+addFormatBtn.addEventListener("click", formatsConfig)
+
+// adicionar/remover formatos, precisando ter ao menos 1 e limite de 3
+function formatsConfig(event) {
+    if (event.target == addFormatBtn && currentFormat < FORMAT_QUANTITY - 1 ) {
+        formats[currentFormat+1].style.display = "block";
+        currentFormat += 1;
+        
+    } else if (event.target == removeFormatBtn && currentFormat > 0) {
+        formats[currentFormat].style.display = "none";
+        currentFormat -= 1;
+    }
+
+}
