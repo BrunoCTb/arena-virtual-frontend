f7f0c9a5121dc01541a02b6c271b4458da546f5a
BrunoCTmb <zazaduza10@gmail.com>
2025-03-18 18:05:20 -0300

    Add html and basic components for 'create-team'

--- components/js/header.js
+++ components/js/header.js
@@ -13,6 +13,7 @@ const NAV_BAR_CONTENT = `
         <ul class="app-header-links">
             <a href="#"><li>Campeonatos</li></a>
             <a href="/pages/create-tournament.html"><li>Criar campeonato (provisório)</li></a>
+            <a href="/pages/create-team.html"><li>Criar Time (provisório)</li></a>
             <a href="#"><li>Times</li></a>
             <a href="#"><li>Ranking</li></a>
         </ul>
