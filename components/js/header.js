5d088e0a0693e21aa883dcbd8e33ef5981df20bf
BrunoCTmb <zazaduza10@gmail.com>
2025-03-25 17:17:42 -0300

    add content (base) for show all players

--- components/js/header.js
+++ components/js/header.js
@@ -12,7 +12,7 @@ const NAV_BAR_CONTENT = `
         
         <ul class="app-header-links">
             <a href="/pages/all-tournaments.html"><li>Campeonatos</li></a>
-            <a href="/pages/profile/invites.html"><li>Convites - times (provisório)</li></a>
+            <a href="/pages/all-players.html"><li>Jogadores</li></a>
             <a href="/pages/all-teams.html""><li>Times</li></a>
             <a href="#"><li>Ranking</li></a>
         </ul>
