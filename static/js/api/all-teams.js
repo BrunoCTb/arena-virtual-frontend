3c045a4a61c1528fb4e1b44878c23596afbde903
BrunoCTmb <zazaduza10@gmail.com>
2025-04-13 12:54:19 -0300

    Feature => Get all teams from db and add to 'all-teams' page

---
+++ static/js/api/all-teams.js
@@ -0,0 +1,61 @@
+function getAllTeams() {
+    const apiUrl = "http://localhost:8080/team/all";
+
+    fetchPerso(apiUrl, {
+        method: "GET"
+    })
+    .then(teams => {
+        // quantidade encontrada
+        document.getElementById("teams-quantity-found").innerHTML = teams.length;
+
+        let teamsList = document.getElementsByClassName("list-of-teams")[0];
+        teamsList.innerHTML = "";
+
+        console.log(teamsList);
+        
+        for (t of teams) {
+            html = `<div class="team-data">
+                    <div class="team-card">
+                        <div class="tm-image">
+                            <img src="#" alt="">
+                        </div>
+                        <div class="tm-content">
+                            <div class="tm-title">
+                                <h3>${t.name}</h3>
+                            </div>
+                            <div class="tm-info">
+                                <div>
+                                    <span>Jogadores:</span>
+                                    <span>${t.quantity}</span>
+                                </div>
+                                <div>
+                                    <span>Aberto para convite:</span>
+                                    <span>${t.openToInvite}</span>
+                                </div>
+                                <div>
+                                    <span>Campeonatos participando:</span>
+                                    <span>${null}</span>
+                                </div>
+                                <div>
+                                    <span>Criador:</span>
+                                    <span>${t.createdBy.username}</span>
+                                </div>
+                            </div>
+                            <div class="tm-actions">
+                                <button>Ver detalhes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+
+            teamsList.innerHTML += html;
+        }
+
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    
+}
+
+getAllTeams();
