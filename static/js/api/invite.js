9cd32c5823986c19d91cb2a1a6c73a550fda2d96
BrunoCTmb <zazaduza10@gmail.com>
2025-05-20 18:12:55 -0300

    Code: create a method to execute set and get invites

--- static/js/api/invite.js
+++ static/js/api/invite.js
@@ -1,7 +1,5 @@
-let username = "username123"
-
 // MOSTRAR OS CONVITES RECEBIDOS NO HTML
-function setReceivedInvite() {
+function setReceivedInvite(username) {
 	let receivedUrl = `http://localhost:8080/${username}/invites/received`;
 	
 	fetchPerso(receivedUrl, {
@@ -11,37 +9,37 @@ function setReceivedInvite() {
 		let receivedInvitations = document.getElementsByClassName("received-invitations")[0];
 
 		if (data.length == 0) {
-			document.getElementById("not-received-sent-invitations").innerHTML = "Nenhum convite enviado no momento"
+			document.getElementById("not-found-received-invitations").innerHTML = "Nenhum convite enviado no momento"
 			return;
 		}
-
+		
 		for (let received of data) {
 			let inviteHTML = let = `<div class="received-invite">
 			<div class="received-invite-content">
-				<div class="received-invite-data">
-					<h2>Solicitação do ${received.invitedTarget.username}</h2>
+			<div class="received-invite-data">
+					<h2>Solicitação do ${received.invitedBy.username}</h2>
 					<span>Recebido em ${received.createdAt}</span>
 					<span>Time: ${received.teamTarget.name}</span>
 				</div>
 				<div class="received-invite-status">
-					<span>${received.inviteStatus}</span>
+				<span>${received.inviteStatus}</span>
 				</div>
 				</div>
 				<div class="received-invite-actions">
-					<button>Aceitar</button>
+				<button>Aceitar</button>
 					<button>Rejeitar</button>
 				</div>
-			</div>`
-
+				</div>`
+				
 			receivedInvitations.innerHTML += inviteHTML;
 		}
 	})
 }
 
 // MOSTRAR OS CONVITES ENVIADOS NO HTML
-function setSentInvite() {
+function setSentInvite(username) {
 	let sentUrl = `http://localhost:8080/${username}/invites/sent`;
-
+	
 	fetchPerso(sentUrl, {
 		method: "GET"
 	})
@@ -52,12 +50,12 @@ function setSentInvite() {
 			document.getElementById("not-found-sent-invitations").innerHTML = "Nenhum convite enviado no momento"
 			return;
 		}
-
+		
 		for (let invite of data) {
 			let inviteHTML = let = `<div class="sent-invite">
 				<div class="sent-invite-content">
 					<div class="sent-invite-data">
-						<h2>Convite para ${invite.invitedBy.username}</h2>
+					<h2>Convite para ${invite.invitedTarget.username}</h2>
 						<span>Enviado em ${invite.createdAt}</span>
 						<span>Time: ${invite.teamTarget.name}</span>
 					</div>
@@ -68,19 +66,26 @@ function setSentInvite() {
 				<div class="sent-invite-actions">
 					<button>Cancelar</button>
 				</div>
-			</div>`
+				</div>`
 
-			sentInvitations.innerHTML += inviteHTML;
+				sentInvitations.innerHTML += inviteHTML;
 		}
 	})
 }
 
-// Enviar convite para o player entrar no time
-function sendTeamInvite(param) {
-	
+function setInvites() {
+	url = "http://localhost:8080/user/auth/get"
+
+	fetchPerso(url, {
+		method: "GET"
+	})
+	.then(user => {
+		setReceivedInvite(user.username);
+		setSentInvite(user.username);
+	})
+	.catch(e => {
+		console.log("e: " + e);
+	})
 }
 
-
-setReceivedInvite();
-setSentInvite();
-
+setInvites();
