c56e4a867be62268c8efd6d1694528addc2e4572
BrunoCTmb <zazaduza10@gmail.com>
2025-09-03 22:54:46 -0300

    Feat: Handle backend not working error, redirecting to specifc page

--- static/js/api/auth.js
+++ static/js/api/auth.js
@@ -1,22 +1,33 @@
 // fetch personalizado
 function fetchPerso(url, options = {}) {
+    
     const token = localStorage.getItem("authToken");
-
+    
     if (!options.headers) {
         options.headers = {};
     }
-
+    
     // enviar o token com o prefix bearer para o backend identificar e fazer o replace
     if (token) {
         options.headers['Authorization'] = "Bearer " + token;
     }
+    
+    
+    return fetch(url, options)
+        .then(response => {
+            if (response.status == 403) {
+                localStorage.removeItem("authToken");
+                window.location.href = "/pages/login.html";
+                throw new Error("Não autorizado");
+            } else {
+                
+            }
+            
+            return response; // <-- devolve o Response inteiro
+        })
+        .catch(error => {
+            console.log("backend not working...");
 
-    return fetch(url, options).then(response => {
-        if (response.status == 403) {
-            localStorage.removeItem("authToken");
-            window.location.href = "/pages/login.html";
-            throw new Error("Não autorizado");
-        }
-        return response; // <-- devolve o Response inteiro
-    });
+            window.location.href = "/pages/error/backend-not-working.html"
+        });
 }
